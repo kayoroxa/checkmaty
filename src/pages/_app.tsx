4c37576bc8c9a4906966bb2c4831b00c04ba38a3
Caio <kayoroxa@gmail.com>
2023-05-05 15:57:51 -0300

    title page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import '../styles/globals.css'
@@ -8,6 +9,11 @@ export const queryClient = new QueryClient()
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>CheckMaty</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
       <ReactQueryDevtools />
     </QueryClientProvider>
