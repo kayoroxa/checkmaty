a28d6ccddc90940b7d840cf1ca1821b64824f8c6
Caio <kayoroxa@gmail.com>
2023-05-06 02:25:47 -0300

    make backup

--- middlewares.js
+++ middlewares.js
@@ -1,9 +1,26 @@
 require('dotenv').config({ path: './.env.local' })
 const fs = require('fs')
 
+function makeCopy() {
+  const jsonString = fs.readFileSync(process.env.JSON_DB_PATH, 'utf8')
+
+  fs.writeFileSync(
+    `${process.env.JSON_DB_PATH.replace('.json', '_copy.json')}`,
+    jsonString
+  )
+}
+
 module.exports = (req, res, next) => {
   const entriesQuery = Object.entries(req.query)
   const hasNull = entriesQuery.some(([_, value]) => value === 'null')
+
+  if (
+    req.method === 'DELETE' ||
+    req.method === 'PATCH' ||
+    req.method === 'PUT'
+  ) {
+    makeCopy()
+  }
 
   if (hasNull) {
     const jsonDb = JSON.parse(fs.readFileSync(process.env.JSON_DB_PATH, 'utf8'))
